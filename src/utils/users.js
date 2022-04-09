95e3ba84ee42df1129b00cd8d94cb5623e9b00fe
iliasanthimos <96637213+iliasanthimos@users.noreply.github.com>
2022-04-09 03:45:39 +0300

    Location link fix and remove user

--- src/utils/users.js
+++ src/utils/users.js
@@ -35,7 +35,7 @@ const addUser = ({id, username, room}) => {
 
 const removeUser = (id) => {
     const index = users.findIndex((user) => user.id === id)
-    if(index !== -1) {
+    if(!index !== -1) {
         return users.splice(index, 1)[0]
     }
 }
